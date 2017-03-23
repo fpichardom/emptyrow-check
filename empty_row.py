6188ff7cf9b58d5e0bb97ed70bee6f52e47e0d35
Fritz <fpichardom@yahoo.com>
2017-03-23 15:00:16 -0230

    Modified to select a specific character

--- empty_row.py
+++ empty_row.py
@@ -2,26 +2,18 @@
 import csv
 
 file1 = input('File 1 Name: ')
-n = input('Number of empty fields allowed:\n')
-n = int(n)
 delimiter = input('File delimiter: ')
 with open(file1+'.csv','r') as file_1,\
-    open('empty.csv','w', newline = '') as empty,\
-    open('goodrecords.csv','w', newline = '') as good:
+    open('selection.csv','w', newline = '') as empty:
     reader = csv.reader(file_1, delimiter = delimiter)
     list1 = list(reader)
     header = list1[0]
     EmptyWriter = csv.writer(empty)
-    GoodWriter = csv.writer(good)
     EmptyWriter.writerow(header)
-    
+
     for i in list1[1:]:
-        if i.count('') > n:
+        if i[1]=="." and i[2]==".":
             EmptyWriter.writerow(i)
-            list1.remove(i)
-    for i in list1:
-        GoodWriter.writerow(i)
-        
         
             
         
